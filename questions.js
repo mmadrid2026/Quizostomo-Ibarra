41ec72c4553f69ce31dff2d8d0f77c8bcf361971
mmadrid2026 <117584927+mmadrid2026@users.noreply.github.com>
2023-02-10 19:28:58 +0800

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,58 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "ang importante ay?",
+    answer: "mahalaga",
+    options: [
+      "mahalaga",
+      "significant",
+      "ikaw",
+      "masaya ka"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Anong mangyayari pag naging stars tayo?",
+    answer: "U DREW STARS AROUND MY SCARS BUT NOW I'M BLEEDING",
+    options: [
+      "nearly all the elements in the human body were made in a star and many have come through several supernovas",
+      "magkakaroon ng something bituin us",
+      "magiging star ka sa star ng pasko",
+      "U DREW STARS AROUND MY SCARS BUT NOW I'M BLEEDING"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Taylor Swift is?",
+    answer: "ME! HEE-HEE",
+    options: [
+      "an American Singer-Songwriter",
+      "it's me, hi! i'm the problem, it's me!",
+      "ME! HEE-HEE",
+      "Inang Taylor Marie Batumbakal"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is Love?",
+    answer: "I WANNA KNOW KNOW KNOW KNOW",
+    options: [
+      "it encompasses a range of strong and positive emotional and mental states, from the most sublime virtue or good habit, the deepest interpersonal affection, to the simplest pleasure.",
+      "I WANNA KNOW KNOW KNOW KNOW",
+      "burning red but it's golden like daylight",
+      "siya"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "Xhare Mo Lang",
+    options: [
+      "eXtensible Markup Language",
+      "Xhare Mo Lang",
+      "eXTra Multi-Program Language",
+      "Xmall Medium Large"
+    ]
+  },
+];
